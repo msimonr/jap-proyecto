2e1e13b2ea54863fda9b6eb99e8665d1d74145fe
msimonr <mauricio.simon.r@gmail.com>
2021-08-10 08:55:27 -0300

    Productos-Entrega 1 listo

--- js/products.js
+++ js/products.js
@@ -1,6 +1,30 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function(e) {
+    // Agregado entrega 1
+    getJSONData(PRODUCTS_URL).then(productos => {
+        prodData = productos.data;
+        let htmlContentToAppend = '';
+        for (prod of prodData) {
+            htmlContentToAppend += `
+            <a href="category-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + prod.imgSrc + `" alt="` + prod.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">` + prod.name + `</h4>
+                            <small class="text-muted">` + prod.soldCount + ` vendidos</small>
+                        </div>
+                        <p class="mb-1">` + prod.description + `</p>
+                        <p class="mb-1 precio">` + prod.currency + ` ` + prod.cost + `</p>      
+                    </div>
+                </div>
+            </a>
+            `
+        }
+        document.getElementById('product-list-container').innerHTML = htmlContentToAppend;
+    });
 });
