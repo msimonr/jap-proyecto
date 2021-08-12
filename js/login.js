d725e51d137bc8f2115d98c8a848ad99751ed878
msimonr <mauricio.simon.r@gmail.com>
2021-08-12 08:41:17 -0300

    Login listo

--- js/login.js
+++ js/login.js
@@ -1,6 +1,14 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e) {
 
 });
+
+// Agregado Entrega1
+function login() {
+    let user = document.getElementById("inputEmail").value;
+
+    //Guardar datos de sesion.
+    sessionStorage.setItem('user', user);
+}
