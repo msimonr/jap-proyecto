8c3fd8c542e5c295dde76e328081bd22fb5cf782
msimonr <mauricio.simon.r@gmail.com>
2021-08-26 14:59:54 -0300

    act

--- js/products.js
+++ js/products.js
@@ -22,39 +22,48 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
 
     //Botones de orden
-    $('#mayorPrecio').click(function(){
+    $('#mayorPrecio').click(function() {
         sortList(P_HIGH, listaProductos);
         showList(listaProductos);
     });
 
-    $('#menorPrecio').click(function(){
+    $('#menorPrecio').click(function() {
         sortList(P_LOW, listaProductos);
         showList(listaProductos);
     });
 
-    $('#mayorRelevancia').click(function(){
+    $('#mayorRelevancia').click(function() {
         sortList(P_REL, listaProductos);
         showList(listaProductos);
     });
 
     // Botones de filtrado
 
-    $('#filter').click(function(){
-        let min = $('#rangeFilterMin').value;
-        let max = $('#rangeFilterMax').value;
-        if(min !== undefined || max !== undefined){
-            $('.producto').forEach(element => {
-                if(min !== undefined && element.cost < min){
-                    
+    $('#filter').click(function() {
+        let min = parseInt($('#rangeFilterMin').val());
+        let max = parseInt($('#rangeFilterMax').val());
+        console.log(min + ' : ' + max);
+        if (min !== NaN || max !== NaN) {
+            console.log('hola');
+            $('.producto').each(function() {
+                console.log($(this));
+                let precio = listaProductos.find(elem => elem.name === $(this).attr('id')).cost;
+                console.log(precio);
+                if ((min !== undefined && precio < min) || (max !== undefined && precio > max)) {
+                    $(this).hide();
+                } else {
+                    //mostrarlo.
+                    $(this).show();
                 }
-            });
-        }
+            }); //fin each
+        } //fin if undefined
+    }); //fin click.
+
+    $('#clear').click(function() {
+        $('#rangeFilterMin').val('');
+        $('#rangeFilterMax').val('');
+        $('.producto').each(() => $(this).show());
     });
-
-    
-    // $('#clear').click(function(){
-
-    // });
 
 });
 
@@ -63,7 +72,7 @@ function showList(prodData) {
     let htmlContentToAppend = '';
     for (prod of prodData) {
         htmlContentToAppend += `
-            <a href="product-info.html" class="list-group-item list-group-item-action producto">
+            <a href="product-info.html" class="list-group-item list-group-item-action producto" id="${prod.name}">
                 <div class="row">
                     <div class="col-3">
                         <img src="` + prod.imgSrc + `" alt="` + prod.description + `" class="img-thumbnail">
@@ -83,29 +92,28 @@ function showList(prodData) {
     document.getElementById('product-list-container').innerHTML = htmlContentToAppend;
 }
 
-function sortList(criterio, productos){
-    switch (criterio){
+function sortList(criterio, productos) {
+    switch (criterio) {
         case P_HIGH:
-            productos.sort((a,b)=>{
-                if(a.cost > b.cost){return -1;}
-                if(a.cost < b.cost){return 1;}
+            productos.sort((a, b) => {
+                if (a.cost > b.cost) { return -1; }
+                if (a.cost < b.cost) { return 1; }
                 return 0;
             });
             break;
         case P_LOW:
-            productos.sort((a,b)=>{
-                if(a.cost > b.cost){return 1;}
-                if(a.cost < b.cost){return -1;}
+            productos.sort((a, b) => {
+                if (a.cost > b.cost) { return 1; }
+                if (a.cost < b.cost) { return -1; }
                 return 0;
             });
             break;
         case P_REL:
-            productos.sort((a,b)=>{
-                if(a.soldCount > b.soldCount){return -1;}
-                if(a.soldCount < b.soldCount){return 1;}
+            productos.sort((a, b) => {
+                if (a.soldCount > b.soldCount) { return -1; }
+                if (a.soldCount < b.soldCount) { return 1; }
                 return 0;
             });
             break;
-    } 
+    }
 }
-
